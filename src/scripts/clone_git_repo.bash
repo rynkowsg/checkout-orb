5e5f14b52112eec54d647d59a79d9c460ca0f6e1
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-03-23 21:44:36 -0300

    build: Update shell-gr to v0.2.0

--- src/scripts/clone_git_repo.bash
+++ src/scripts/clone_git_repo.bash
@@ -21,18 +21,18 @@ if [ -z "${SHELL_GR_DIR:-}" ]; then
   SCRIPT_PATH="$([[ ! "${SCRIPT_PATH_1}" =~ /bash$ ]] && readlink -f "${SCRIPT_PATH_1}" || echo "")"
   SCRIPT_DIR="$([ -n "${SCRIPT_PATH}" ] && (cd "$(dirname "${SCRIPT_PATH}")" && pwd -P) || echo "")"
   ROOT_DIR="$([ -n "${SCRIPT_DIR}" ] && (cd "${SCRIPT_DIR}/../.." && pwd -P) || echo "/tmp")"
-  SHELL_GR_DIR="${ROOT_DIR}/.github_deps/rynkowsg/shell-gr@b6f2f78"
+  SHELL_GR_DIR="${ROOT_DIR}/.github_deps/rynkowsg/shell-gr@v0.2.0"
 fi
 # Library Sourcing
-# shellcheck source=.github_deps/rynkowsg/shell-gr@b6f2f78/lib/color.bash
+# shellcheck source=.github_deps/rynkowsg/shell-gr@v0.2.0/lib/color.bash
 source "${SHELL_GR_DIR}/lib/color.bash"
-# shellcheck source=.github_deps/rynkowsg/shell-gr@b6f2f78/lib/circleci.bash
+# shellcheck source=.github_deps/rynkowsg/shell-gr@v0.2.0/lib/circleci.bash
 source "${SHELL_GR_DIR}/lib/circleci.bash" # fix_home_in_old_images, print_common_debug_info
-# shellcheck source=.github_deps/rynkowsg/shell-gr@b6f2f78/lib/git_checkout_advanced.bash
+# shellcheck source=.github_deps/rynkowsg/shell-gr@v0.2.0/lib/git_checkout_advanced.bash
 source "${SHELL_GR_DIR}/lib/git_checkout_advanced.bash" # git_checkout_advanced
-# shellcheck source=.github_deps/rynkowsg/shell-gr@b6f2f78/lib/git_lfs.bash
+# shellcheck source=.github_deps/rynkowsg/shell-gr@v0.2.0/lib/git_lfs.bash
 source "${SHELL_GR_DIR}/lib/git_lfs.bash" # setup_git_lfs
-# shellcheck source=.github_deps/rynkowsg/shell-gr@b6f2f78/lib/ssh.bash
+# shellcheck source=.github_deps/rynkowsg/shell-gr@v0.2.0/lib/ssh.bash
 source "${SHELL_GR_DIR}/lib/ssh.bash" # setup_ssh
 
 #################################################
